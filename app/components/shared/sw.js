da1bc5c6e2f2eb4c82fd8bd1f7228ee9b1772477
Aruna Tebel <arunatebel@hotmail.com>
2015-12-12 13:46:38 +0530

    Remove old cache

--- app/components/shared/sw.js
+++ app/components/shared/sw.js
@@ -3,7 +3,7 @@
  */
 this.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open('v2').then(function (cache) {
+        caches.open('v2.1').then(function (cache) {
             return cache.addAll([
                 '/app/view1/view1.html',
                 '/app/view2/view2.html'
@@ -18,11 +18,25 @@ this.addEventListener('fetch', function (event) {
         return fetch(event.request);
     }).then(function (r) {
         response = r;
-        caches.open('v2').then(function (cache) {
+        caches.open('v2.1').then(function (cache) {
             cache.put(event.request, response);
         });
         return response.clone();
     }).catch(function () {
         return caches.match('/sw-test/gallery/myLittleVader.jpg');
     }));
+});
+
+this.addEventListener('activate', function (event) {
+    var cacheWhitelist = ['v2.1'];
+
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    );
 });
