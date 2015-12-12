34124325f1003280d20782df5e2259b4cc14a0f0
Aruna Tebel <arunatebel@hotmail.com>
2015-12-12 13:40:10 +0530

    changed sw caching resources

--- app/components/shared/sw.js
+++ app/components/shared/sw.js
@@ -5,16 +5,8 @@ this.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open('v1').then(function (cache) {
             return cache.addAll([
-                '/sw-test/',
-                '/sw-test/index.html',
-                '/sw-test/style.css',
-                '/sw-test/app.js',
-                '/sw-test/image-list.js',
-                '/sw-test/star-wars-logo.jpg',
-                '/sw-test/gallery/',
-                '/sw-test/gallery/bountyHunters.jpg',
-                '/sw-test/gallery/myLittleVader.jpg',
-                '/sw-test/gallery/snowTroopers.jpg'
+                '/app/view1/view1.html',
+                '/app/view2/view2.html'
             ]);
         })
     );
