73d20c98adac77ed1a6f3af59e64de15280316a2
Aruna Tebel <arunatebel@hotmail.com>
2015-12-14 15:50:54 +0530

    Update manifest.js

--- app/manifest.js
+++ app/manifest.js
@@ -11,6 +11,6 @@ var OFFLINK_STATIC_CACHE = [
 var OFFLINK_DYNAMIC_CACHE = {
     //'/app/issues.html': '/app/common/common_issues.html',
     //'/app/profile.html': '/app/common/common_profile.html',
-    '/app/project.html': '/app/common/common_project.html'
+    //'/app/project.html': '/app/common/common_project.html'
     //'/app/projects.html': '/app/common/common_project.html'
 };
